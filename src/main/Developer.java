fac2acd2b775d2a55deec096d524af4309367c7c
Ana-Maria Radu <48693018+anamariaradu0@users.noreply.github.com>
2020-03-12 17:56:51 +0200

    Add files via upload

---
+++ src/main/Developer.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Developer extends Person {
+    ArrayList<String> skills;
+
+    public ArrayList<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(ArrayList<String> skills) {
+        this.skills = skills;
+    }
+}
---
+++ src/main/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    String company;
+    int bonus;
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+}
