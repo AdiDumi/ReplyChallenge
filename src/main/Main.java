521dd8b9b17a1db446fa91dc2fd9ef667e6982ae
AdiDumi <adrian.dumitrescu99@stud.acs.upb.ro>
2020-03-12 19:33:47 +0200

    clear main

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,16 +7,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //      TESTING FOR UNION + INTERSECTION
-        ArrayList<String> skills1 = new ArrayList<String>();
-        skills1.add("frumos");
-        skills1.add("destept");
-        ArrayList<String> skills2 = new ArrayList<String>();
-        skills2.add("urat");
-        skills2.add("destept");
-        Developer p1 = new Developer(skills1);
-        Developer p2 = new Developer(skills2);
-        p1.workPotential(p2);
 
         GameInputLoader gameInputLoader = new GameInputLoader(args[0], args[1]);
         GameInput gameInput = gameInputLoader.load();
@@ -27,12 +17,12 @@ public class Main {
         nrDevs = gameInput.getNrDev();
         nrPMs = gameInput.getNrPM();
         List<String> allOffice = new ArrayList<>();
-        List<String> allDevCompany = new ArrayList<>();
-        List<String> allDevSkills = new ArrayList<>();
-        List<Integer> allPMBonus = new ArrayList<>();
-        List<Integer> allDevBonus = new ArrayList<>();
-        List<String> allPMCompany = new ArrayList<>();
-        List<Integer> allDevNrSkill = new ArrayList<>();
+        List<String> allDevCompany;
+        List<String> allDevSkills;
+        List<Integer> allPMBonus;
+        List<Integer> allDevBonus;
+        List<String> allPMCompany;
+        List<Integer> allDevNrSkill;
         allDevCompany = gameInput.getMallDevCompany();
         allDevBonus = gameInput.getMallDevBonus();
         allDevNrSkill = gameInput.getMallDevNrSkill();
@@ -40,6 +30,7 @@ public class Main {
         ArrayList<String> devSkills = new ArrayList<>();
         allPMCompany = gameInput.getMallPMCompany();
         allPMBonus = gameInput.getMallPMBonus();
+
 
         int k = 0;
         for (int i = 0; i < nrDevs; ++i) {
